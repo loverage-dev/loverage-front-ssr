d150095a1ce1d1643c525fc90ecb7672ffb0e1bc
zetton110 <zetton110@gmail.com>
2020-06-27 00:03:13 +0900

    カテゴリーメニューの実装

--- components/atom/category-menu.vue
+++ components/atom/category-menu.vue
@@ -1,20 +1,118 @@
 <template>
     <div class="a-category-menu">
     <ul class="a-category-menu__list">
-        <li class="a-category-menu__list-item"><a href="">出会い</a></li>
-        <li class="a-category-menu__list-item"><a href="">片思い</a></li>
-        <li class="a-category-menu__list-item"><a href="">友達・同僚</a></li>
-        <li class="a-category-menu__list-item"><a href="">デート</a></li>
-        <li class="a-category-menu__list-item"><a href="">告白</a></li>
-        <li class="a-category-menu__list-item"><a href="">彼氏・彼女</a></li>
-        <li class="a-category-menu__list-item"><a href="">元カレ・元カノ</a></li>
-        <li class="a-category-menu__list-item"><a href="">結婚</a></li>
-        <li class="a-category-menu__list-item"><a href="">結婚生活</a></li>
-        <li class="a-category-menu__list-item"><a href="">別れ</a></li>
-        <li class="a-category-menu__list-item"><a href="">浮気・不倫</a></li>
-        <li class="a-category-menu__list-item"><a href="">失恋</a></li>
-        <li class="a-category-menu__list-item"><a href="">性・SEX</a></li>
-        <li class="a-category-menu__list-item"><a href="">その他</a></li>
+        <li class="a-category-menu__list-item">
+          <nuxt-link 
+            :to="{
+              path: encodeURI('/category/出会い')
+            }">
+            出会い
+          </nuxt-link>
+        </li>
+        <li class="a-category-menu__list-item">
+          <nuxt-link 
+            :to="{
+              path: encodeURI('/category/片想い')
+            }">
+            片想い
+          </nuxt-link>
+        </li>
+        <li class="a-category-menu__list-item">
+          <nuxt-link 
+            :to="{
+              path: encodeURI('/category/友達・同僚')
+            }">
+            友達・同僚
+          </nuxt-link>
+        </li>
+        <li class="a-category-menu__list-item">
+          <nuxt-link 
+            :to="{
+              path: encodeURI('/category/デート')
+            }">
+            デート
+          </nuxt-link>
+        </li>
+        <li class="a-category-menu__list-item">
+          <nuxt-link 
+            :to="{
+              path: encodeURI('/category/告白')
+            }">
+            告白
+          </nuxt-link>
+        </li>
+        <li class="a-category-menu__list-item">
+                    <nuxt-link 
+            :to="{
+              path: encodeURI('/category/彼氏・彼女')
+            }">
+            彼氏・彼女
+          </nuxt-link>
+        </li>
+        <li class="a-category-menu__list-item">
+          <nuxt-link 
+            :to="{
+              path: encodeURI('/category/元カレ・元カノ')
+            }">
+            元カレ・元カノ
+          </nuxt-link>
+          </li>
+        <li class="a-category-menu__list-item">
+          <nuxt-link 
+            :to="{
+              path: encodeURI('/category/結婚')
+            }">
+            結婚
+          </nuxt-link>
+        </li>
+        <li class="a-category-menu__list-item">
+          <nuxt-link 
+            :to="{
+              path: encodeURI('/category/結婚生活')
+            }">
+            結婚生活
+          </nuxt-link>
+        </li>
+        <li class="a-category-menu__list-item">
+          <nuxt-link 
+            :to="{
+              path: encodeURI('/category/別れ')
+            }">
+            別れ
+          </nuxt-link>
+        </li>
+        <li class="a-category-menu__list-item">
+          <nuxt-link 
+            :to="{
+              path: encodeURI('/category/浮気・不倫')
+            }">
+            浮気・不倫
+          </nuxt-link>
+        </li>
+        <li class="a-category-menu__list-item">
+          <nuxt-link 
+            :to="{
+              path: encodeURI('/category/失恋')
+            }">
+            失恋
+          </nuxt-link>
+        </li>
+        <li class="a-category-menu__list-item">
+          <nuxt-link 
+            :to="{
+              path: encodeURI('/category/性・SEX')
+            }">
+            性・SEX
+          </nuxt-link>
+        </li>
+        <li class="a-category-menu__list-item">
+          <nuxt-link 
+            :to="{
+              path: encodeURI('/category/その他')
+            }">
+            その他
+          </nuxt-link>
+        </li>
     </ul>
     </div>
 </template>
