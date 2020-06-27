817bfd64471dc6520df69bc1eb7c26f451e7d651
zetton110 <zetton110@gmail.com>
2020-06-27 23:16:28 +0900

    エラーページのベース作成

---
+++ layouts/error.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+      <div class="t-desktop-one-column">
+      <div class="t-desktop-one-column__inner">
+        <h2 class="a-heading-mini">「Love」 + 「Average」 = 「Loverage」</h2>
+        <p class="p-about__sentences">
+          
+
+        </p>
+    <h2 v-if="error.statusCode === 404" class="error-message">
+      リクエストされたURLは存在しません
+    </h2>
+    <h2 v-else class="error-message">
+      エラーが発生しました
+    </h2>
+    <nuxt-link class="btn" to="/">
+      トップページへ戻る
+    </nuxt-link>
+      </div>
+    </div>
+  </div>
+</template>
+ 
+<style scoped>
+</style>
+ 
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default: null
+    }
+  },
+  layout: 'info'
+}
+</script>
---
+++ layouts/info.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="t-wrapper">
+    <Header />
+    <nuxt/>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from '~/components/organism/header.vue'
+import Footer from '~/components/organism/footer.vue'
+import CategoryMenu from '~/components/atom/category-menu.vue'
+
+export default {
+  components: {
+    Header,
+    Footer,
+    CategoryMenu,
+  },
+}
+</script>
+
+<style>
+</style>
