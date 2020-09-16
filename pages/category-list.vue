f66a3fbf3eb47153c3dd46c90bfd5cfa681be14b
zetton110 <zetton110@gmail.com>
2020-09-16 23:54:38 +0900

    category-listページのその他の記事取得を共通化

--- pages/category-list.vue
+++ pages/category-list.vue
@@ -230,10 +230,24 @@
     </div>
   </div>
   <div class="o-list-block-area">
-      <ListBlock :title="'ピックアップ記事'" :articles="articles_sub1" />
-      <ListBlock :title="'話題の記事'" :articles="articles_sub2" />
-      <ListBlock :title="'編集者のおすすめ'" :articles="articles_sub3" />
-      <ListBlock :title="'最新の記事'" :articles="articles_sub4" />
+      <ListBlock
+      :title="'編集者のおすすめ'"
+      :articles="dipsItemsEditors"
+      :articlesHidden="dipsItemsHiddenEditors" 
+      v-on:clicked="showNextEditors"
+      :isEndPage="isEndPageEditors" />
+      <ListBlock
+      :title="'話題の記事'"
+      :articles="dipsItemsHotTopic"
+      :articlesHidden="dipsItemsHiddenHotTopic"
+      v-on:clicked="showNextHotTopic"
+      :isEndPage="isEndPageHotTopic" />
+      <ListBlock
+      :title="'ピックアップ記事'"
+      :articles="dipsItemsFeatured"
+      :articlesHidden="dipsItemsHiddenFeatured"
+      v-on:clicked="showNextFeatured"
+      :isEndPage="isEndPageFeatured" />
   </div>
 </div>
 
@@ -259,19 +273,58 @@ export default {
       class: 'p-category-list'
     }
   },
+  mounted(){
+    this.$store.dispatch('shared/editors_pick/resetPageCount')
+    this.$store.dispatch('shared/hot_topic/resetPageCount')
+    this.$store.dispatch('shared/featured/resetPageCount')
+  },
   methods: {
-     ...mapActions('pages/category-list',['getArticles'])
+    showNextHotTopic(){ this.$store.dispatch('shared/hot_topic/showNextPage')},
+    showNextEditors(){ this.$store.dispatch('shared/editors_pick/showNextPage')},
+    showNextFeatured(){　this.$store.dispatch('shared/featured/showNextPage')},
   },
   computed:{
-    ...mapState("pages/category-list",{
-      articles_sub1: state => state.articles_sub1,
-      articles_sub2: state => state.articles_sub2,
-      articles_sub3: state => state.articles_sub3,
-      articles_sub4: state => state.articles_sub4
+    ...mapState("shared/hot_topic",{
+      hot_topics: state => state.articles
+    }),
+    ...mapState("shared/featured",{
+      featureds: state => state.articles
+    }),
+    ...mapState("shared/editors_pick",{
+      editors_picks: state => state.articles
+    }),
+    ...mapState("shared/categories",{
+      categoryList: state => state.categoryList
+    }),
+    ...mapGetters({
+      // ピックアップ記事
+      articleCountFeatured:'shared/featured/articleCount',
+      dipsItemsFeatured:'shared/featured/dipsItems',
+      dipsItemsHiddenFeatured:'shared/featured/dipsItemsHidden',
+      isEndPageFeatured:'shared/featured/isEndPage',
+      pageCountFeatured:'shared/featured/pageCount',
+      // 編集者のおすすめ
+      articleCountEditors:'shared/editors_pick/articleCount',
+      dipsItemsEditors:'shared/editors_pick/dipsItems',
+      dipsItemsHiddenEditors:'shared/editors_pick/dipsItemsHidden',
+      isEndPageEditors:'shared/editors_pick/isEndPage',
+      pageCountEditors:'shared/editors_pick/pageCount',
+      // 話題の記事
+      articleCountHotTopic:'shared/hot_topic/articleCount',
+      dipsItemsHotTopic:'shared/hot_topic/dipsItems',
+      dipsItemsHiddenHotTopic:'shared/hot_topic/dipsItemsHidden',
+      isEndPageHotTopic:'shared/hot_topic/isEndPage',
+      pageCountHotTopic:'shared/hot_topic/pageCount',
     })
   },
   async asyncData({ store }) {
-    await store.dispatch('pages/category-list/getArticles')
+
+    if(!store.state.categoryList){
+      await store.dispatch('shared/categories/getCategoryList', null, { root: true })
+    }
+    await store.dispatch('shared/editors_pick/getArticles')
+    await store.dispatch('shared/hot_topic/getArticles')
+    await store.dispatch('shared/featured/getArticles')
   }
 };
 </script>
