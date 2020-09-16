e6008b89294e743f50f03be5c408d3cbdf2e059a
zetton110 <zetton110@gmail.com>
2020-09-16 23:59:54 +0900

    rankingページのその他の記事取得を共通化

--- pages/ranking.vue
+++ pages/ranking.vue
@@ -12,31 +12,31 @@
                 閲覧数
               </nuxt-link>
             </li>
-            <li class="p-ranking__tabs-item" :class="{ 'is-active': mode === 'likes' }">
+            <li class="p-ranking__tabs-item" :class="{ 'is-active': mode === 'favorite' }">
             <nuxt-link 
               :to="{ path: encodeURI('/ranking/')}"
-              @click.native="toRankingLikes()">
+              @click.native="toRankingFavorite()">
               いいね数
             </nuxt-link>
             </li>
         </ul>
         <div class="o-list">
-            <client-only>
-              <ListItemRanking 
-                :article="article" 
-                :rank="index + 1" 
-                v-for="(article,index) in rankings" 
-                v-bind:key='article.id' />
-            </client-only>
+          <client-only>
+            <ListItemRanking 
+              :article="article" 
+              :rank="index + 1" 
+              v-for="(article,index) in rankings" 
+              v-bind:key='article.id' />
+          </client-only>
         </div>
         <nuxt-link 
           v-if="mode === 'view'" 
           :to="{ path: encodeURI('/ranking/')}" 
-          @click.native="toRankingLikes()">
+          @click.native="toRankingFavorite()">
           <button class="a-button-secondary">いいね数ランキングも見る</button>
         </nuxt-link>
         <nuxt-link 
-          v-if="mode === 'likes'"
+          v-if="mode === 'favorite'"
           :to="{ path: encodeURI('/ranking/')}"
           @click.native="toRankingView()">
           <button class="a-button-secondary">閲覧数ランキングも見る</button>
@@ -44,10 +44,24 @@
       </div>
     </div>
     <div class="o-list-block-area">
-      <ListBlock :title="'最新の記事'" :articles="latests" />
-      <ListBlock :title="'編集者のおすすめ'" :articles="editors_picks" />
-      <ListBlock :title="'話題の記事'" :articles="hot_topics" />
-      <ListBlock :title="'ピックアップ記事'" :articles="featureds" />
+      <ListBlock
+      :title="'編集者のおすすめ'"
+      :articles="dipsItemsEditors"
+      :articlesHidden="dipsItemsHiddenEditors" 
+      v-on:clicked="showNextEditors"
+      :isEndPage="isEndPageEditors" />
+      <ListBlock
+      :title="'話題の記事'"
+      :articles="dipsItemsHotTopic"
+      :articlesHidden="dipsItemsHiddenHotTopic"
+      v-on:clicked="showNextHotTopic"
+      :isEndPage="isEndPageHotTopic" />
+      <ListBlock
+      :title="'ピックアップ記事'"
+      :articles="dipsItemsFeatured"
+      :articlesHidden="dipsItemsHiddenFeatured"
+      v-on:clicked="showNextFeatured"
+      :isEndPage="isEndPageFeatured" />
     </div>
   </div>
 </template>
@@ -73,34 +87,65 @@ export default {
       class: 'p-ranking'
     }
   },
+  mounted(){
+    this.$store.dispatch('shared/editors_pick/resetPageCount')
+    this.$store.dispatch('shared/hot_topic/resetPageCount')
+    this.$store.dispatch('shared/featured/resetPageCount')
+  },
   methods: {
      ...mapActions('pages/rankings',[
-            'getRankingPosts',
             'toRankingView',
-            'toRankingLikes']),
-     ...mapActions('shared/articles',['getOverviewPosts'])
+            'toRankingFavorite']),
+    showNextHotTopic(){ this.$store.dispatch('shared/hot_topic/showNextPage')},
+    showNextEditors(){ this.$store.dispatch('shared/editors_pick/showNextPage')},
+    showNextFeatured(){　this.$store.dispatch('shared/featured/showNextPage')},
   },
   computed:{
-    ...mapState("shared/articles",{
-      featureds: state => state.featured,
-      hot_topics: state => state.hot_topic,
-      editors_picks: state => state.editors_pick,
-      latests: state => state.latest
+    ...mapState("shared/hot_topic",{
+      hot_topics: state => state.articles
+    }),
+    ...mapState("shared/featured",{
+      featureds: state => state.articles
+    }),
+    ...mapState("shared/editors_pick",{
+      editors_picks: state => state.articles
     }),
     ...mapState("pages/rankings",{
       rankings: state => {
         if(state.mode === 'view'){
           return state.rankings_view
         }else{
-          return state.rankings_vote
+          return state.rankings_favorite
         }
       },
       mode: state => state.mode
+    }),
+    ...mapGetters({
+      // ピックアップ記事
+      articleCountFeatured:'shared/featured/articleCount',
+      dipsItemsFeatured:'shared/featured/dipsItems',
+      dipsItemsHiddenFeatured:'shared/featured/dipsItemsHidden',
+      isEndPageFeatured:'shared/featured/isEndPage',
+      pageCountFeatured:'shared/featured/pageCount',
+      // 編集者のおすすめ
+      articleCountEditors:'shared/editors_pick/articleCount',
+      dipsItemsEditors:'shared/editors_pick/dipsItems',
+      dipsItemsHiddenEditors:'shared/editors_pick/dipsItemsHidden',
+      isEndPageEditors:'shared/editors_pick/isEndPage',
+      pageCountEditors:'shared/editors_pick/pageCount',
+      // 話題の記事
+      articleCountHotTopic:'shared/hot_topic/articleCount',
+      dipsItemsHotTopic:'shared/hot_topic/dipsItems',
+      dipsItemsHiddenHotTopic:'shared/hot_topic/dipsItemsHidden',
+      isEndPageHotTopic:'shared/hot_topic/isEndPage',
+      pageCountHotTopic:'shared/hot_topic/pageCount',
     })
   },
   async asyncData({ store }) {
-    await store.dispatch('shared/articles/getOverviewPosts')
     await store.dispatch('pages/rankings/getRankingPosts')
+    await store.dispatch('shared/editors_pick/getArticles')
+    await store.dispatch('shared/hot_topic/getArticles')
+    await store.dispatch('shared/featured/getArticles')
   }
 }
 </script>
