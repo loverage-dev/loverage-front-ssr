e9f9dac494d3405c460169b16f1747ac093e8309
zetton110 <zetton110@gmail.com>
2020-09-17 00:01:21 +0900

    いいねランキングを取得するように変更

--- store/pages/rankings.js
+++ store/pages/rankings.js
@@ -9,22 +9,22 @@ export default {
     actions:{
       async getRankingPosts ({commit}) {
           await Promise.all(
-            [this.$axios.$get(`${ process.env.API_BASE_URL }/api/v1/ranking_view?limit=10`),
-            this.$axios.$get(`${ process.env.API_BASE_URL }/api/v1/ranking_vote?limit=10`)]
+            [this.$axios.$get(`${ process.env.API_BASE_URL }/api/v1/ranking_favorite?limit=10`),
+            this.$axios.$get(`${ process.env.API_BASE_URL }/api/v1/ranking_view?limit=10`)]
           )
           .then(result => {
             let api1Result = result[0]
             let api2Result = result[1]
-            commit("setRankingsView", api1Result.articles);
-            commit("setRankingsVote", api2Result.articles);
+            commit("setRankingFavorite", api1Result.articles);
+            commit("setRankingsView", api2Result.articles);
           }
         )
       },
       toRankingView({commit}){
         commit("setViewMode")
       },
-      toRankingLikes({commit}){
-        commit("setLikesMode")
+      toRankingFavorite({commit}){
+        commit("setFavoriteMode")
       }
     },
     mutations:{
@@ -34,11 +34,14 @@ export default {
       setRankingsVote(state, data){
         state.rankings_vote = data;
       },
+      setRankingFavorite(state, data){
+        state.rankings_favorite = data;
+      },
       setViewMode(state){
         state.mode = 'view'
       },
-      setLikesMode(state){
-        state.mode = 'likes'
+      setFavoriteMode(state){
+        state.mode = 'favorite'
       }
     }
   }
