c24cce0bfd0eaaadd39cde7e76ae1e45790ef707
zetton110 <zetton110@gmail.com>
2020-09-19 04:01:04 +0900

    記事ページのリファクタリング

--- pages/article/_id.vue
+++ pages/article/_id.vue
@@ -8,7 +8,7 @@
       :created_at="article.post.created_at" 
       :age="article.post.user_age"
       :gender="article.post.user_sex"
-      :likesCount="article.post.ref_count"
+      :likesCount="article.post.favorite"
       :viewsCount="article.post.ref_count"
        />
     <div class="p-article__itself">
@@ -22,16 +22,31 @@
         v-if="article.post.tag_list.length != 0"
         :tags="article.post.tag_list" />
     </div>
-    <Answer />
+    <Answer :article="article" />
     <CommentArea v-if="article.comments.contents.length != 0" />
     <ShareLikeBar />
   </article>
   <aside>
     <div class="o-list-block-area">
-      <ListBlock :title="'ピックアップ記事'" :articles="articles_sub1" />
-      <ListBlock :title="'話題の記事'" :articles="articles_sub2" />
-      <ListBlock :title="'編集者のおすすめ'" :articles="articles_sub3" />
-      <ListBlock :title="'最新の記事'" :articles="articles_sub4" />    </div>
+    <ListBlock
+    :title="'編集者のおすすめ'"
+    :articles="dipsItemsEditors"
+    :articlesHidden="dipsItemsHiddenEditors" 
+    v-on:clicked="showNextEditors"
+    :isEndPage="isEndPageEditors" />
+    <ListBlock
+    :title="'話題の記事'"
+    :articles="dipsItemsHotTopic"
+    :articlesHidden="dipsItemsHiddenHotTopic"
+    v-on:clicked="showNextHotTopic"
+    :isEndPage="isEndPageHotTopic" />
+    <ListBlock
+    :title="'ピックアップ記事'"
+    :articles="dipsItemsFeatured"
+    :articlesHidden="dipsItemsHiddenFeatured"
+    v-on:clicked="showNextFeatured"
+    :isEndPage="isEndPageFeatured" />
+     </div>
   </aside>
 </div>
 
@@ -65,17 +80,55 @@ export default {
       class: 'p-article'
     }
   },
+  mounted(){
+    this.$store.dispatch('shared/editors_pick/resetPageCount')
+    this.$store.dispatch('shared/hot_topic/resetPageCount')
+    this.$store.dispatch('shared/featured/resetPageCount')
+  },
+   methods: {
+    showNextHotTopic(){ this.$store.dispatch('shared/hot_topic/showNextPage')},
+    showNextEditors(){ this.$store.dispatch('shared/editors_pick/showNextPage')},
+    showNextFeatured(){　this.$store.dispatch('shared/featured/showNextPage')},
+  },
   computed:{
     ...mapState("pages/article",{
-      article: state => state.article,
-      articles_sub1: state => state.articles_sub1,
-      articles_sub2: state => state.articles_sub2,
-      articles_sub3: state => state.articles_sub3,
-      articles_sub4: state => state.articles_sub4
+      article: state => state.article
     }),
+    ...mapState("shared/hot_topic",{
+      hot_topics: state => state.articles
+    }),
+    ...mapState("shared/featured",{
+      featureds: state => state.articles
+    }),
+    ...mapState("shared/editors_pick",{
+      editors_picks: state => state.articles
+    }),
+    ...mapGetters({
+      // ピックアップ記事
+      articleCountFeatured:'shared/featured/articleCount',
+      dipsItemsFeatured:'shared/featured/dipsItems',
+      dipsItemsHiddenFeatured:'shared/featured/dipsItemsHidden',
+      isEndPageFeatured:'shared/featured/isEndPage',
+      pageCountFeatured:'shared/featured/pageCount',
+      // 編集者のおすすめ
+      articleCountEditors:'shared/editors_pick/articleCount',
+      dipsItemsEditors:'shared/editors_pick/dipsItems',
+      dipsItemsHiddenEditors:'shared/editors_pick/dipsItemsHidden',
+      isEndPageEditors:'shared/editors_pick/isEndPage',
+      pageCountEditors:'shared/editors_pick/pageCount',
+      // 話題の記事
+      articleCountHotTopic:'shared/hot_topic/articleCount',
+      dipsItemsHotTopic:'shared/hot_topic/dipsItems',
+      dipsItemsHiddenHotTopic:'shared/hot_topic/dipsItemsHidden',
+      isEndPageHotTopic:'shared/hot_topic/isEndPage',
+      pageCountHotTopic:'shared/hot_topic/pageCount',
+    })
   },
   async asyncData({ store,route }) {
     await store.dispatch('pages/article/getArticle', {articleId: route.params.id})
+    await store.dispatch('shared/editors_pick/getArticles')
+    await store.dispatch('shared/hot_topic/getArticles')
+    await store.dispatch('shared/featured/getArticles')
   }
 };
 </script>
